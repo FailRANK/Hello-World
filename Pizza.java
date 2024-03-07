d4323dbb67f39dfc8b3fd29f7400972e925a7c21
FailRANK <franknguyen2006@gmail.com>
2024-03-07 11:13:41 -0500

    Added more function

--- Pizza.java
+++ Pizza.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
+import javax.swing.plaf.TreeUI;
+
 public class Pizza {
 
     public static void main(String[] args) {
 
+        Scanner Input = new Scanner(System.in);
+        String Choice;
+
         System.out.println("Welcome to Frank's Pizzeria");
     
-        System.out.println("Welcome to Frank's Pizzeria");
+        System.out.println("Add Pizzas [Y/N]");
+
+        Choice = Input.next();
+
+        System.out.println("Choice")
+
+        if (Choice == "Y") {
+
+            System.out.println("Pizza added");
+
+        } 
 
     }
 
